31b8328ca51928ba6317dd95578b8b27b92d8314
Rain202210 <yu.he@thoughtworks.com>
2023-01-07 16:10:09 +0800

    feat: add redux toolkit example page

--- apps/demoapp/src/pages/_app.tsx
+++ apps/demoapp/src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import next18nextConfig from '../../next-i18next.config';
+import { Provider } from 'react-redux';
+import store from '../store/store';
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
 );
 
 export default appWithTranslation(MyApp, next18nextConfig);
