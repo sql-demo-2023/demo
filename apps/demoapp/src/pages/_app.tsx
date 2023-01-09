9b3c4d538f19eb0590c793efa3f3eb5f65b48e19
Rain202210 <yu.he@thoughtworks.com>
2023-01-09 21:11:36 +0800

    feat: add mui custom theme styled component

--- apps/demoapp/src/pages/_app.tsx
+++ apps/demoapp/src/pages/_app.tsx
@@ -2,11 +2,27 @@ import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import next18nextConfig from '../../next-i18next.config';
 import { Provider } from 'react-redux';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import createEmotionCache from '../styles/theme/createEmotionCache';
+import theme from '../styles/theme/theme';
 import store from '../store/store';
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
-);
+const clientSideEmotionCache = createEmotionCache();
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+const MyApp = (props: MyAppProps) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </Provider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
 
 export default appWithTranslation(MyApp, next18nextConfig);
