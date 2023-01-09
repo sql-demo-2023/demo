2d1d56a7fc3557ce1d2edd440f70e19bf66e5e2f
Rain202210 <yu.he@thoughtworks.com>
2023-01-09 21:15:59 +0800

    fix: delete emotionCache

--- apps/demoapp/src/pages/_app.tsx
+++ apps/demoapp/src/pages/_app.tsx
@@ -3,25 +3,18 @@ import { appWithTranslation } from 'next-i18next';
 import next18nextConfig from '../../next-i18next.config';
 import { Provider } from 'react-redux';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import createEmotionCache from '../styles/theme/createEmotionCache';
 import theme from '../styles/theme/theme';
 import store from '../store/store';
-const clientSideEmotionCache = createEmotionCache();
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-const MyApp = (props: MyAppProps) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+const MyApp = (props: AppProps) => {
+  const { Component, pageProps } = props;
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </Provider>
-      </ThemeProvider>
-    </CacheProvider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
   );
 };
 
