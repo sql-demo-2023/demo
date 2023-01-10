aded9c4991702147ae77208ca55de46775709a37
Rain2022debug <heyu2001cau@gmail.com>
2023-01-10 19:45:30 +0800

    fix: fix redux test fail by adding await

--- apps/demoapp/jest.config.ts
+++ apps/demoapp/jest.config.ts
@@ -1,12 +1,34 @@
 /* eslint-disable */
-export default {
+// export default {
+//   displayName: 'demoapp',
+//   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+//   preset: '../../jest.preset.js',
+//   transform: {
+//     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+//     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+//   },
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+//   coverageDirectory: '../../coverage/apps/demoapp',
+// };
+
+import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
+
+const config: InitialOptionsTsJest = {
   displayName: 'demoapp',
+  preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-  preset: '../../jest.preset.js',
   transform: {
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/demoapp',
+  testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
 };
+
+export default config;
--- apps/demoapp/src/features/counter/__tests__/Counter.spec.tsx
+++ apps/demoapp/src/features/counter/__tests__/Counter.spec.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { makeStore } from '../../../store/store';
 import Counter from '../Counter';
-jest.mock('./counterAPI', () => ({
+jest.mock('../counterAPI', () => ({
   fetchCount: (amount: number) =>
     new Promise<{ data: number }>((resolve) =>
       setTimeout(() => resolve({ data: amount }), 500)
@@ -19,5 +21,91 @@ describe('<Counter />', () => {
       </Provider>
     );
     expect(screen.getByText('0')).toBeInTheDocument();
+  });
+  it('decrements the value', async () => {
+    const user = userEvent.setup();
+    const store = makeStore();
+
+    render(
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+
+    await user.click(screen.getByRole('button', { name: /decrement value/i }));
+
+    expect(screen.getByText('-1')).toBeInTheDocument();
+  });
+  it('increments the value', async () => {
+    const store = makeStore();
+
+    render(
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+
+    await user.click(screen.getByRole('button', { name: /increment value/i }));
+
+    expect(screen.getByText('1')).toBeInTheDocument();
+  });
+
+  it('increments by amount', async () => {
+    const store = makeStore();
+
+    render(
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+
+    await user.type(
+      screen.getByLabelText(/set increment amount/i),
+      '{backspace}5'
+    );
+    await user.click(screen.getByRole('button', { name: /add amount/i }));
+
+    expect(screen.getByText('5')).toBeInTheDocument();
+  });
+
+  it('increments async', async () => {
+    const store = makeStore();
+
+    render(
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+
+    await user.type(
+      screen.getByLabelText(/set increment amount/i),
+      '{backspace}3'
+    );
+    await user.click(screen.getByRole('button', { name: /add async/i }));
+
+    await expect(screen.findByText('3')).resolves.toBeInTheDocument();
+  });
+
+  it('increments if amount is odd', async () => {
+    const store = makeStore();
+
+    render(
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    );
+
+    await user.click(screen.getByRole('button', { name: /add if odd/i }));
+
+    expect(screen.getByText('0')).toBeInTheDocument();
+
+    await user.click(screen.getByRole('button', { name: /increment value/i }));
+    await user.type(
+      screen.getByLabelText(/set increment amount/i),
+      '{backspace}8'
+    );
+    await user.click(screen.getByRole('button', { name: /add if odd/i }));
+
+    await expect(screen.findByText('9')).resolves.toBeInTheDocument();
   });
 });
