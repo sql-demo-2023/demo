507481156572f83f4c81571ff54aa1db8358a256
Rain2022debug <heyu2001cau@gmail.com>
2023-01-10 19:53:28 +0800

    fix: fix jest config

--- apps/demoapp/jest.config.ts
+++ apps/demoapp/jest.config.ts
@@ -1,34 +1,33 @@
 /* eslint-disable */
-// export default {
+export default {
+  displayName: 'demoapp',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/demoapp',
+};
+
+// import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
+
+// const config: InitialOptionsTsJest = {
 //   displayName: 'demoapp',
+//   preset: 'ts-jest',
 //   setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-//   preset: '../../jest.preset.js',
 //   transform: {
 //     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
 //     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
 //   },
 //   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-//   coverageDirectory: '../../coverage/apps/demoapp',
+//   testEnvironment: 'jsdom',
+//   globals: {
+//     'ts-jest': {
+//       tsconfig: 'tsconfig.test.json',
+//     },
+//   },
 // };
 
-import type { InitialOptionsTsJest } from 'ts-jest/dist/types';
-
-const config: InitialOptionsTsJest = {
-  displayName: 'demoapp',
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-  transform: {
-    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
-};
-
-export default config;
+// export default config;
